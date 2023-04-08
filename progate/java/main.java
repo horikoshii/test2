c2dc3e1c42e32249dd185e30eebde7d69d1b1e2e
GitHubのユーザ名horikoshii <achiga9696@gmail.com>
2023-04-08 08:34:06 +0000

    Javadojo

--- progate/java/main.java
+++ progate/java/main.java
@@ -6,25 +6,39 @@ class Main {
     
     System.out.println("何人分の情報を入力しますか：");
     int n = scanner.nextInt();
+    
+    int maxAge = 0;
+    
+    int totalAge = 0;
+    
+    
     for (int i = 0; i < n; i++){
-      System.out.println(i + 1 + "人目");
+      System.out.println(i + 1 +"人目");
       
       System.out.println("名前：");
       String firstName = scanner.next();
+      
       System.out.println("名字：");
       String lastName = scanner.next();
       
-      String name = firstName + " " + lastName;
-     
       System.out.println("年齢：");
       int age = scanner.nextInt();
       
+      if (age > maxAge){
+        maxAge = age;
+      }
+      
+      totalAge += age;
+      
       System.out.println("身長(m)：");
       double height = scanner.nextDouble();
+      
       System.out.println("体重(kg)：");
       double weight = scanner.nextDouble();
       
-      Person.printData(firstName,lastName,age,height,weight);
+      Person.printData(firstName, lastName, age, height, weight);
     }
+    System.out.println("最高年齢は" + maxAge + "歳です");
+    System.out.println("平均年齢は" + totalAge / n + "歳です");
   }
 }
