dbf2ecdeb3ed13ff2909c306f81b0eaaf941d75f
GitHubのユーザ名horikoshii <achiga9696@gmail.com>
2023-04-14 02:02:25 +0000

    car

--- progate/javadojoII/main.java
+++ progate/javadojoII/main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    Bicycle bicycle = new Bicycle("ビアンキ","緑");
+    Scanner scanner = new Scanner(System.in);
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
     System.out.println("【自転車の情報】");
     bicycle.printData();
     System.out.println("-----------------");
-    bicycle.run(10);
+    System.out.print("走る距離を入力してください：");
+    int bicycleDistance = scanner.nextInt();
+    bicycle.run(bicycleDistance);
+    
+    System.out.println("=================");
+    Car car = new Car("フェラーリ", "赤");
+    System.out.println("【車の情報】");
+    car.printData();
   }
 }
