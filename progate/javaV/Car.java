ebb729d338aced818471f16f931f41e09719f58f
GitHubのユーザ名horikoshii <achiga9696@gmail.com>
2023-04-18 08:36:30 +0000

    java

--- progate/javaV/Car.java
+++ progate/javaV/Car.java
@@ -1,12 +1,30 @@
 class Car extends Vehicle {
   private int fuel = 50;
 
+  Car(String name, String color) {
+    super(name, color);
+  }
+
   public int getFuel() {
     return this.fuel;
   }
-  
-  // printDataメソッドを定義してください
-  
+
+  public void printData() {
+    super.printData();
+    System.out.println("ガソリン量：" + this.fuel + "L");
+  }
+
+  public void run(int distance) {
+    System.out.println(distance + "km走ります");
+    if (distance <= this.fuel) {
+      this.distance += distance;
+      this.fuel -= distance;
+    } else {
+      System.out.println("ガソリンが足りません");
+    }
+    System.out.println("走行距離：" + this.distance + "km");
+    System.out.println("ガソリン量：" + this.fuel + "L");
+  }
 
   public void charge(int litre) {
     System.out.println(litre + "L給油します");
--- progate/javaV/main.java
+++ progate/javaV/main.java
@@ -3,25 +3,27 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    Car car = new Car();
-    car.setName("フェラーリ");
-    car.setColor("赤");
-    Bicycle bicycle = new Bicycle();
-    bicycle.setName("ビアンキ");
-    bicycle.setColor("緑");
+    Car car = new Car("フェラーリ", "赤");
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
 
     System.out.println("【車の情報】");
     car.printData();
-    // 「ガソリン量：○○L」と出力している箇所を消してください
-    System.out.println("ガソリン量：" + car.getFuel() + "L");
+    System.out.println("-----------------");
+    System.out.print("走る距離を入力してください：");
+    int carDistance = scanner.nextInt();
+    car.run(carDistance);
     
     System.out.println("-----------------");
     System.out.print("給油する量を入力してください：");
     int litre = scanner.nextInt();
     car.charge(litre);
-
+    
     System.out.println("=================");
     System.out.println("【自転車の情報】");
     bicycle.printData();
+    System.out.println("-----------------");
+    System.out.print("走る距離を入力してください：");
+    int bicycleDistance = scanner.nextInt();
+    bicycle.run(bicycleDistance);
   }
 }
